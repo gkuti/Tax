6cf1ac6d6f5ce954668688e0d6685ae68776bcf3
Kuti gbolahan <kuti.gbolahan@andela.com>
2016-08-16 09:26:28 +0100

    initiliaze project

---
+++ app/src/main/java/com/taxsolutions/taxify/CustomTextView.java
@@ -0,0 +1,49 @@
+package com.taxsolutions.taxify;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by kutigbolahan on 25/07/2016.
+ */
+public class CustomTextView extends TextView {
+    private static final String TAG = "TextView";
+
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.TextViewPlus);
+        String customFont = a.getString(R.styleable.TextViewPlus_customFont);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface typeface = null;
+        try {
+            typeface = Typeface.createFromAsset(ctx.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Unable to load typeface: " + e.getMessage());
+            return false;
+        }
+
+        setTypeface(typeface);
+        return true;
+    }
+}
